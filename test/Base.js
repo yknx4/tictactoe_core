78526f278a0d9973f77813ff3512d53f70008b84
Alejandro Figueroa <yknx.4.b@gmail.com>
2016-09-22 02:17:37 -0500

    Clean Base Test

--- test/Base.js
+++ test/Base.js
@@ -5,19 +5,16 @@ import Base from '../lib/Base'
 
 describe('tictactoe_core', function() {
   it('load', function() {
-
     test
       .function(tictactoeCore)
         .hasName('tictactoeCore')
       .object(tictactoeCore())
         .isInstanceOf(Game)
 
-
   })
 
   describe('Base class', function() {
     it('constructor() - throw a TypeError', function() {
-
       var newBase = function(){
         new Base()
       }
@@ -26,9 +23,8 @@ describe('tictactoe_core', function() {
         .exception(newBase)
         .is(new TypeError("Cannot construct Base isnstances directly"))
 
+    })
 
-    });
-
-  });
+  })
 
 })
