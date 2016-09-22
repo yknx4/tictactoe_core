7a4d47e9b3e0d0d5c0cd375c847ac03248261bdd
Alejandro Figueroa <yknx.4.b@gmail.com>
2016-09-22 02:25:44 -0500

    Create Game Class

---
+++ src/Game.js
@@ -0,0 +1,20 @@
+import Base from './Base';
+
+export default class Game extends Base {
+
+  constructor(){
+    super()
+    this._playerCount = 2
+    this._fieldsToWin = 3
+  }
+
+  get playerCount(){
+    return this._playerCount;
+  }
+
+  get fieldsToWin(){
+    return this._fieldsToWin;
+  }
+
+
+}
