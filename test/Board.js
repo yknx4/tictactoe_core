aa45bca3fe8b2ea2ae997c47177224d9c1f561b7
Alejandro Figueroa <yknx.4.b@gmail.com>
2016-09-22 22:08:16 -0500

    Test for winner in Board

--- test/Board.js
+++ test/Board.js
@@ -5,49 +5,98 @@ import Board from '../lib/Board'
 describe('Board class', function() {
   it('constructor() - create a new Board', function() {
     test
-      .function(Board)
-        .hasName('Board')
-      .object(new Board())
-        .isInstanceOf(Board)
+    .function(Board)
+    .hasName('Board')
+    .object(new Board())
+    .isInstanceOf(Board)
   })
 
   it('constructor() - set default width', function() {
     let board = new Board()
     test
-      .number(board.width)
-      .is(3)
+    .number(board.width)
+    .is(3)
   })
 
   it('constructor() - set default height', function() {
     let board = new Board()
     test
-      .number(board.height)
-      .is(3)
+    .number(board.height)
+    .is(3)
   })
 
   it('constructor(5) - set width', function() {
     let board = new Board(5)
     test
-      .number(board.width)
-      .is(5)
+    .number(board.width)
+    .is(5)
   })
 
   it('constructor(5) - set height same as width', function() {
     let board = new Board(5)
     test
-      .number(board.height)
-      .is(board.width)
-      .is(5)
+    .number(board.height)
+    .is(board.width)
+    .is(5)
   })
 
   it('constructor(5,3) - set width and height', function() {
     let board = new Board(5, 3)
     test
-      .number(board.width)
-      .is(5)
+    .number(board.width)
+    .is(5)
 
     test
-      .number(board.height)
-      .is(3)
+    .number(board.height)
+    .is(3)
+  })
+
+  it("play('X', 1, 1) - should set (1,1) on field as 'X'", function() {
+    let board = new Board(3, 3)
+    board.play('X', 1, 1)
+
+    test
+    .string(board.field[1][1])
+    .is('X')
+  })
+
+  it("play('X', 3, 3) - should throw an error in a 3x3 Board", function() {
+    let board = new Board(3, 3)
+    var invalidPlay = function() {
+      board.play('X', 3, 3)
+    }
+
+    test
+      .exception(invalidPlay)
+      .is(new RangeError("(3,3) is outside the field. (3x3)"))
+  })
+
+  it("play('Y', 1, 1) - should throw an error when (1,1) is already set", function() {
+    let board = new Board(3, 3)
+    var invalidPlay = function() {
+      board.play('X', 1, 1)
+      board.play('Y', 1, 1)
+    }
+
+    test
+      .exception(invalidPlay)
+      .is(new Error("(1,1) is already set to 'X'."))
+  })
+
+  it("checkWinner('X', 1, 1, 3) - should win when there are 3 same points vertically", function() {
+    let board = new Board(3, 3)
+    board.play('X', 1, 0)
+    board.play('X', 1, 1)
+    board.play('X', 1, 2)
+
+    test
+      .bool(board.checkWinner('X', 1, 0, 3))
+      .isTrue()
+    test
+      .bool(board.checkWinner('X', 1, 1, 3))
+      .isTrue()
+    test
+      .bool(board.checkWinner('X', 1, 2, 3))
+      .isTrue()
   })
 })
