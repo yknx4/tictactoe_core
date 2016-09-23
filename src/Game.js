0abb449399a76470a2c588cd0e2291abe06c608f
Jorge Figueroa <yknx.4.b@gmail.com>
2016-09-23 10:41:00 -0500

    Test new Game construction with added parameters

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,41 @@
 import Base from './Base';
+import Board from './Board'
+import Player from './Player'
+import ParameterValidation from './validation/ParameterValidation.js'
 
 export default class Game extends Base {
 
-  constructor() {
+  constructor(boardWidth, boardHeigth, numOfPlayers, fieldsToWin) {
     super()
-    this._playerCount = 2
-    this._fieldsToWin = 3
+    this._numOfPlayers = numOfPlayers || 2
+    this._fieldsToWin = fieldsToWin || 3
+    this._board = new Board(boardWidth, boardHeigth)
+    this._players = []
+  }
+
+  get numOfPlayers() {
+    return this._numOfPlayers;
   }
 
   get playerCount() {
-    return this._playerCount;
+    return this._players.length
   }
 
   get fieldsToWin() {
     return this._fieldsToWin;
+  }
+
+  addPlayer(player) {
+    ParameterValidation.validateType(player, Player, 'player')
+    this._validatePlayerCount()
+    this._players.push(player)
+  }
+
+  _validatePlayerCount() {
+    let playersCapped = this.playerCount >= this.numOfPlayers
+    if (playersCapped) {
+      throw new RangeError('Game is full.')
+    }
   }
 
 }
--- test/Game.js
+++ test/Game.js
@@ -1,19 +1,36 @@
 /* eslint-env node, mocha */
 import test from 'unit.js'
 import Game from '../lib/Game'
+import Board from '../lib/Board'
 
 describe('Game class', function() {
-  it('constructor() - set default player count', function() {
-    let game = new Game()
+  it('constructor(3, 3) - set default number of players', function() {
+    let game = new Game(3, 3)
     test
-      .number(game.playerCount)
+      .number(game.numOfPlayers)
       .is(2)
   })
 
-  it('constructor() - set default fields to win count', function() {
-    let game = new Game()
+  it('constructor(3, 3) - set default fields to win count', function() {
+    let game = new Game(3, 3)
     test
       .number(game.fieldsToWin)
       .is(3)
+  })
+
+  it('constructor(3, 3) - create a 3x3 Board', function() {
+    let game = new Game(3, 3)
+    test
+      .object(game._board)
+      .isInstanceOf(Board)
+  })
+
+  it('constructor(3, 3, 5, 2) - set number of players to 5 and fields to win to 2', function() {
+    let game = new Game(3, 3, 5, 2)
+    test
+      .number(game.numOfPlayers)
+        .is(5)
+      .number(game.fieldsToWin)
+        .is(2)
   })
 })
