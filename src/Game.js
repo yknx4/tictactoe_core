590516c6b6ab72cf16cdde3939a0db738bfbefc5
Jorge Figueroa <yknx.4.b@gmail.com>
2016-09-23 17:17:56 -0500

    WIP

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@ import Base from './Base';
 import Board from './Board'
 import Player from './Player'
 import ParameterValidation from './validation/ParameterValidation.js'
+import WinnerChecker from './WinnerChecker'
 import _ from 'underscore'
 
 export default class Game extends Base {
@@ -12,6 +13,7 @@ export default class Game extends Base {
     this._fieldsToWin = fieldsToWin || 3
     this._board = new Board(boardWidth, boardHeigth)
     this._players = []
+    this._winnerCheckers = {}
     this._validateFieldsToWin(_.max([boardWidth, boardHeigth]))
   }
 
@@ -38,12 +40,26 @@ export default class Game extends Base {
     ParameterValidation.validateType(player, Player, 'player')
     this._validatePlayerCount()
     this._players.push(player)
+    this._winnerCheckers[player.id] = new WinnerChecker(this._board, player.id, this.fieldsToWin)
   }
 
   _validatePlayerCount() {
     let playersCapped = this.playerCount >= this.numOfPlayers
     if (playersCapped) {
       throw new RangeError('Game is full.')
+    }
+  }
+
+  play(playerId, x, y) {
+    this._validateHasAllPlayers()
+    this._board.play(playerId, x, y)
+    return this._winnerCheckers[playerId].checkWinner(x, y)
+  }
+
+  _validateHasAllPlayers() {
+    let playersCapped = this.playerCount >= this.numOfPlayers
+    if (!playersCapped) {
+      throw new RangeError('Game cannot start without all players.')
     }
   }
 
