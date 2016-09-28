fedbcdaf394253993d28f69eae1e5db10e28f763
Ale Figueroa <yknx.4.b@gmail.com>
2016-09-27 22:07:15 -0500

    Create play interaction in Game

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@ import Base from './Base';
 import Board from './Board'
 import Player from './Player'
 import ParameterValidation from './validation/ParameterValidation.js'
+import WinnerChecker from './WinnerChecker'
 import _ from 'underscore'
 
 export default class Game extends Base {
@@ -12,6 +13,7 @@ export default class Game extends Base {
     this._fieldsToWin = fieldsToWin || 3
     this._board = new Board(boardWidth, boardHeigth)
     this._players = []
+    this._winnerCheckers = {}
     this._validateFieldsToWin(_.max([boardWidth, boardHeigth]))
   }
 
@@ -36,8 +38,18 @@ export default class Game extends Base {
 
   addPlayer(player) {
     ParameterValidation.validateType(player, Player, 'player')
+    let playerExists = !_.isUndefined(this.getPlayer(player.id))
+    if (playerExists) {
+      console.warn(`Player ${player.name} is already in the game.`)
+      return
+    }
     this._validatePlayerCount()
     this._players.push(player)
+    this._winnerCheckers[player.id] = new WinnerChecker(this._board, player.id, this.fieldsToWin)
+  }
+
+  getPlayer(playerId) {
+    return _.findWhere(this._players, {id: playerId});
   }
 
   _validatePlayerCount() {
@@ -45,6 +57,32 @@ export default class Game extends Base {
     if (playersCapped) {
       throw new RangeError('Game is full.')
     }
+  }
+
+  play(playerId, x, y) {
+    this._validateHasAllPlayers()
+    this._board.play(playerId, x, y)
+    this._checkWinners(playerId, x, y)
+  }
+
+  _validateHasAllPlayers() {
+    let playersCapped = this.playerCount >= this.numOfPlayers
+    if (!playersCapped) {
+      throw new RangeError('Game cannot start without all players.')
+    }
+  }
+
+  _checkWinners(playerId, x, y) {
+    let winnerListenerExists = !_.isUndefined(this._onWinnerListener)
+    if (this._winnerCheckers[playerId].checkWinner(x, y) && winnerListenerExists) {
+      this._onWinnerListener(this.getPlayer(playerId))
+    } else if (!winnerListenerExists) {
+      console.warn('There is no onWinnerListener attached.')
+    }
+  }
+
+  set onWinnerListener(listener) {
+    this._onWinnerListener = listener
   }
 
 }
